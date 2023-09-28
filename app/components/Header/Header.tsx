a77342de4ef947d0717f3229aa61aac830560acb
Kiana Yu <qianya1@ualberta.ca>
2023-09-27 21:06:47 -0600

    Update home page

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -6,8 +6,16 @@ import '../Header/header.css'
 
 export default function Header() {
 
+  const scrollHome = () =>{
+    const targetPostion = 0;
+    window.scrollTo({
+      top : targetPostion,
+      behavior: 'smooth'
+    });
+  };
+
   const scrollToPosition = () =>{
-    const targetPostion = 500;
+    const targetPostion = 900;
     window.scrollTo({
       top : targetPostion,
       behavior: 'smooth'
@@ -15,15 +23,15 @@ export default function Header() {
   };
 
   const scrollStudent = () => {
-    const targetPostion2 = 1200
+    const targetPostion2 = 1650
     window.scroll({
       top:targetPostion2,
       behavior: 'smooth'
     })
   }
 
-  const scrollAbout = () =>{
-    const targetPostion3 = 2000
+  const scrollContact = () =>{
+    const targetPostion3 = 2500
     window.scroll({
       top:targetPostion3,
       behavior:'smooth'
@@ -34,27 +42,32 @@ export default function Header() {
   return (
 
     <header className="Header_CISD">
-      <div className="CSID_LOGO">
+      {/* <div className="CSID_LOGO">
         <img src="/School_logo.png" alt="UofA" className='CSID_LOGO_img' />
       </div>
-      <div className='CSID_LOGO'><img src="/x.png" alt="CISD" className='x_img' /></div>
-      <div className='CSID_LOGO'><img src="/CISD_logo.jpg" alt="CISD" className='CISD_LOGO_img' /></div>
+      <div className='CSID_LOGO'><img src="/x.png" alt="CISD" className='x_img' /></div> */}
+      <div className='CSID_LOGO'>
+        <img src="/CISD_logo_sq.png" alt="CISD" className='CISD_LOGO_img' /></div>  
+
+      3
+        
       <nav className="Navigation_item">
         
         <ul>
-          <li><a onClick={scrollToPosition}>Contact Us</a></li>
-          <li><a onClick={scrollStudent}>Student</a></li>
-          <li><a onClick={scrollAbout}>About Us</a></li>
+          <li><a onClick={scrollHome}>主页 </a></li>
+          <li><a onClick={scrollToPosition}>关于我们</a></li>
+          <li><a onClick={scrollStudent}>加入我们</a></li>
+          <li><a onClick={scrollContact}>活动预告</a></li>
         </ul>
       </nav>
-    <div className="actions">
+    {/* <div className="actions">
       <div className="dark-mode">
         Dark Mode
       </div>
       <div className="join-us">
         Join Us
       </div>
-    </div>
+    </div> */}
     </header>
 
 
