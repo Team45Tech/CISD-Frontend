3b92f0d3d37b496a1f9f653627f63b7159da6b4c
Kiana Yu <qianya1@ualberta.ca>
2023-10-07 00:26:49 -0600

    Update footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className='footer'>
+       
       <div className='email_container'>
         <Link href="/">
           <img src="/CISD_logo_sq.png" alt="CISD" className='logo_img' />
@@ -17,7 +18,6 @@ export default function Footer() {
         <a href='/'>Legal</a>
       </div> */}
       
-      {/* 这个东西不知道为什么在全局右下角，你改一下吧 */}
       <div className='copyright_container'>
         <p>&copy; 2023 By Team56Tech.</p> 
       </div>
